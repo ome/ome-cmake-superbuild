bcd9a0bfcb811fb9bd6960f0d59c59a991c75299
Roger Leigh <rleigh@codelibre.net>
2015-12-17 20:24:47 +0000

    boost-1.60: New package

---
+++ packages/boost-1.60/build.cmake
@@ -0,0 +1,85 @@
+# Build boost
+include("${EP_SCRIPT_CONFIG}")
+include("${GENERIC_CMAKE_ENVIRONMENT}")
+
+if (CONFIG MATCHES Rel)
+  set(BOOST_VARIANT release)
+  set(BZIP2_BINARY bz2)
+  set(ZLIB_BINARY zlib)
+else()
+  set(BOOST_VARIANT debug)
+  set(BZIP2_BINARY bz2d)
+  set(ZLIB_BINARY zlibd)
+endif()
+
+if(WIN32)
+
+  message(STATUS "Running ./b2 install
+--prefix=${BIOFORMATS_EP_INSTALL_DIR}
+--lib=${WINDOWS_LIB_DIR}
+--without-python
+cxxflags=${CMAKE_CXX_FLAGS}
+linkflags=${CMAKE_SHARED_LINKER_FLAGS}
+toolset=${BOOST_TOOLSET}
+variant=${BOOST_VARIANT}
+address-model=${EP_PLATFORM_BITS}
+link=shared
+runtime-link=shared
+threading=multi
+-d+2
+-sBZIP2_BINARY=${BZIP2_BINARY}
+-sBZIP2_INCLUDE=${WINDOWS_INCLUDE_DIR}
+-sBZIP2_LIBPATH=${WINDOWS_LIB_DIR}
+-sZLIB_BINARY=${ZLIB_BINARY}
+-sZLIB_INCLUDE=${WINDOWS_INCLUDE_DIR}
+-sZLIB_LIBPATH=${WINDOWS_LIB_DIR}")
+
+  execute_process(COMMAND ./b2 install
+                               "--prefix=${WINDOWS_INSTALL_DIR}"
+                               "--lib=${WINDOWS_LIB_DIR}"
+                               --without-python
+                               "cxxflags=${CMAKE_CXX_FLAGS}"
+                               "linkflags=${CMAKE_SHARED_LINKER_FLAGS}"
+                               "toolset=${BOOST_TOOLSET}"
+                               "variant=${BOOST_VARIANT}"
+                               "address-model=${EP_PLATFORM_BITS}"
+                               "link=shared"
+                               "runtime-link=shared"
+                               "threading=multi"
+                               "-d+2"
+                               "-sBZIP2_BINARY=${BZIP2_BINARY}"
+                               "-sBZIP2_INCLUDE=${WINDOWS_INCLUDE_DIR}"
+                               "-sBZIP2_LIBPATH=${WINDOWS_LIB_DIR}"
+                               "-sZLIB_BINARY=${ZLIB_BINARY}"
+                               "-sZLIB_INCLUDE=${WINDOWS_INCLUDE_DIR}"
+                               "-sZLIB_LIBPATH=${WINDOWS_LIB_DIR}"
+                  WORKING_DIRECTORY "${SOURCE_DIR}"
+                  RESULT_VARIABLE build_result)
+
+  # Boost installs the DLLs into lib; move to bin for consistency.
+  file(GLOB BOOST_DLLS "${BIOFORMATS_EP_LIB_DIR}/*boost*.dll")
+  foreach(dll ${BOOST_DLLS})
+    get_filename_component(dllbase "${dll}" NAME)
+    file(RENAME "${dll}" "${BIOFORMATS_EP_BIN_DIR}/${dllbase}")
+  endforeach()
+
+else(WIN32)
+
+  message(STATUS "Building boost (Unix) with toolset=${BOOST_TOOLSET} cxxflags=${CMAKE_CXX_FLAGS} linkflags=${CMAKE_SHARED_LINKER_FLAGS}")
+
+  execute_process(COMMAND ./b2 install
+                               "cxxflags=${CMAKE_CXX_FLAGS}"
+                               "linkflags=${CMAKE_SHARED_LINKER_FLAGS}"
+                               "link=shared"
+                               "runtime-link=shared"
+                               "threading=multi"
+                               "toolset=${BOOST_TOOLSET}"
+                               "-d+2"
+                  WORKING_DIRECTORY "${SOURCE_DIR}"
+                  RESULT_VARIABLE build_result)
+
+endif(WIN32)
+
+if (build_result)
+  message(FATAL_ERROR "boost: Build failed")
+endif()
---
+++ packages/boost-1.60/superbuild.cmake
@@ -0,0 +1,59 @@
+# boost superbuild
+
+# Set dependency list
+ome_add_dependencies(boost-1.60 THIRD_PARTY_DEPENDENCIES zlib bzip2 icu)
+
+if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+  # VS 10.0
+  if(NOT MSVC_VERSION VERSION_LESS 1600 AND MSVC_VERSION VERSION_LESS 1700)
+    set(BOOST_TOOLSET msvc-10.0)
+  # VS 11.0
+  elseif(NOT MSVC_VERSION VERSION_LESS 1700 AND MSVC_VERSION VERSION_LESS 1800)
+    set(BOOST_TOOLSET msvc-11.0)
+  # VS 12.0
+  elseif(NOT MSVC_VERSION VERSION_LESS 1800 AND MSVC_VERSION VERSION_LESS 1900)
+    set(BOOST_TOOLSET msvc-12.0)
+  # VS 14.0
+  elseif(NOT MSVC_VERSION VERSION_LESS 1900 AND MSVC_VERSION VERSION_LESS 2000)
+    set(BOOST_TOOLSET msvc-14.0)
+  else()
+    set(BOOST_TOOLSET msvc)
+  endif()
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+  set(BOOST_TOOLSET clang)
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  set(BOOST_TOOLSET gcc)
+else()
+  message(FATAL_ERROR "Unsupported Boost toolset for compiler type ${CMAKE_CXX_COMPILER_ID}.  Please report this deficiency.")
+endif()
+
+# Notes:
+# Builds boost without Boost.Python (not currently used by Bio-Formats)
+
+ExternalProject_Add(${EP_PROJECT}
+  ${BIOFORMATS_EP_COMMON_ARGS}
+  URL "http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2"
+  URL_HASH "SHA512=7c851b3fc2b322ff05d642d9cf03e7c30c5f04d5cf0579c99046b1ec708901c58a3d349031dfe24591f5b88c1e664b6a0d40abea6cce89abb52080c02eb725df"
+  SOURCE_DIR "${EP_SOURCE_DIR}"
+  INSTALL_DIR ""
+  CONFIGURE_COMMAND
+    ${CMAKE_COMMAND}
+    ${boost_CLANG_ARG}
+    "-DSOURCE_DIR:PATH=${EP_SOURCE_DIR}"
+    "-DBOOST_TOOLSET=${BOOST_TOOLSET}"
+    "-DCONFIG:INTERNAL=$<CONFIG>"
+    "-DEP_SCRIPT_CONFIG=${EP_SCRIPT_CONFIG}"
+    -P ${CMAKE_CURRENT_LIST_DIR}/configure.cmake
+  BUILD_IN_SOURCE 1
+  BUILD_COMMAND ${CMAKE_COMMAND}
+    "-DSOURCE_DIR:PATH=${EP_SOURCE_DIR}"
+    "-DEP_INSTALL_DIR:PATH=${BIOFORMATS_EP_INSTALL_DIR}"
+    "-DBOOST_TOOLSET=${BOOST_TOOLSET}"
+    "-DBOOST_BITS=${BOOST_BITS}"
+    "-DCONFIG:INTERNAL=$<CONFIG>"
+    "-DEP_SCRIPT_CONFIG=${EP_SCRIPT_CONFIG}"
+    -P "${CMAKE_CURRENT_LIST_DIR}/build.cmake"
+  INSTALL_COMMAND ""
+  DEPENDS
+    ${EP_PROJECT}-prerequisites
+)
