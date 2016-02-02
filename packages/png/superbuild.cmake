c3646c958d8634967510affecc4e4f647baa1b91
Roger Leigh <rleigh@dundee.ac.uk>
2016-02-02 17:32:21 +0000

    png, zlib: Use sourceforge rather than heanet mirror

--- packages/png/superbuild.cmake
+++ packages/png/superbuild.cmake
@@ -13,7 +13,7 @@ if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${EP_PROJECT})
 
   ExternalProject_Add(${EP_PROJECT}
     ${BIOFORMATS_EP_COMMON_ARGS}
-    URL "ftp://ftp.heanet.ie/mirrors/download.sourceforge.net/pub/sourceforge/l/li/libpng/libpng16/1.6.21/libpng-1.6.21.tar.xz"
+    URL "http://downloads.sourceforge.net/project/libpng/libpng16/1.6.21/libpng-1.6.21.tar.xz"
     URL_HASH "SHA512=84f644d455cf90ad721a098fa9e0fcfa6df22a12625faad1c36bf2353ab2315040c7cbd05522c05937c3a31b1c9dd7821cbe29c4a5ba27dec3de92c81ef9ed32"
     SOURCE_DIR "${EP_SOURCE_DIR}"
     BINARY_DIR "${EP_BINARY_DIR}"
