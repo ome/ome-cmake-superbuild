ff3994b7522684aa62615f3b9eb3fcac7cead96b
Roger Leigh <rleigh@codelibre.net>
2016-02-24 16:42:15 +0000

    Set BIOFORMATS_HOME when running tests on Windows

--- helpers/cmake_test.cmake
+++ helpers/cmake_test.cmake
@@ -2,6 +2,11 @@
 include("${EP_SCRIPT_CONFIG}")
 include("${GENERIC_CMAKE_ENVIRONMENT}")
 
+if(WIN32)
+  # Install path introspection doesn't work on Windows with static libraries.
+  set(ENV{BIOFORMATS_HOME} "${CMAKE_INSTALL_PREFIX}")
+endif()
+
 execute_process(COMMAND "${CMAKE_CTEST_COMMAND}" -C "${CONFIG}" -V
                 WORKING_DIRECTORY "${EP_BUILD_DIR}"
                 RESULT_VARIABLE test_result)
