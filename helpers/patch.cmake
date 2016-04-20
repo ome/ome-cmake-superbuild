a4e3900e9c609c59a04f6ab7efced392fd223053
Roger Leigh <rleigh@dundee.ac.uk>
2016-04-20 11:09:05 +0000

    helpers: Convert patch line endings on Windows

--- helpers/patch.cmake
+++ helpers/patch.cmake
@@ -7,16 +7,22 @@ file(STRINGS "${PATCH_DIR}/series" patches)
 foreach(patch ${patches})
   message(STATUS "Applying patch ${patch}")
 
+  set(PATCH_OPTIONS -p1 -E)
+
   if(MSVC)
-    set(PATCH_OPTIONS --binary)
+    execute_process(COMMAND todos
+                    COMMAND patch ${PATCH_OPTIONS}
+                    WORKING_DIRECTORY "${SOURCE_DIR}"
+                    INPUT_FILE "${PATCH_DIR}/${patch}"
+                    RESULT_VARIABLE patch_result)
+  else()
+    execute_process(COMMAND patch ${PATCH_OPTIONS}
+                    WORKING_DIRECTORY "${SOURCE_DIR}"
+                    INPUT_FILE "${PATCH_DIR}/${patch}"
+                    RESULT_VARIABLE patch_result)
   endif()
 
-  execute_process(COMMAND patch -p1 -E ${PATCH_OPTIONS}
-                  WORKING_DIRECTORY "${SOURCE_DIR}"
-                  INPUT_FILE "${PATCH_DIR}/${patch}"
-                  RESULT_VARIABLE patch_result)
-
   if(patch_result)
-    message(FATAL_ERROR "cmake: Patch failed")
+    message(FATAL_ERROR "Patch ${patch} failed")
   endif()
 endforeach()
