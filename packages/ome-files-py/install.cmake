b2e73aa1888e5642d56cf222f1de0030f6da801e
simleo <simleo@crs4.it>
2017-01-17 17:23:51 +0000

    ome-files-py: skip build in the install step

--- packages/ome-files-py/install.cmake
+++ packages/ome-files-py/install.cmake
@@ -4,7 +4,7 @@ include("${GENERIC_CMAKE_ENVIRONMENT}")
 
 file(TO_NATIVE_PATH "${OME_EP_INSTALL_DIR}" NATIVE_INSTALL_DIR)
 
-execute_process(COMMAND python setup.py install
+execute_process(COMMAND python setup.py install --skip-build
   "--prefix=${NATIVE_INSTALL_DIR}"
   WORKING_DIRECTORY "${SOURCE_DIR}"
   RESULT_VARIABLE install_result)
