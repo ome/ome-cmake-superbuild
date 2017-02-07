1f8d2eadbc64880324ef7442fbdcfe2f157eeb3d
simleo <simleo@crs4.it>
2017-02-07 13:13:41 +0000

    ome-files-py: add lib64 paths to PYTHONPATH

--- helpers/cmake_environment.cmake
+++ helpers/cmake_environment.cmake
@@ -18,7 +18,8 @@ if(WIN32)
        "${OME_EP_TOOL_CACHE}/*/*/site-packages"
        "${OME_EP_TOOL_DIR}/*/site-packages"
        "${OME_EP_TOOL_DIR}/*/*/site-packages"
-       "${OME_EP_LIB_DIR}/*/site-packages")
+       "${OME_EP_INSTALL_DIR}/lib/*/site-packages"
+       "${OME_EP_INSTALL_DIR}/lib64/*/site-packages")
   foreach(dir ${python_dirs})
     file(TO_NATIVE_PATH "${dir}" dir)
     if(PYTHONPATH)
@@ -53,7 +54,8 @@ else()
        "${OME_EP_TOOL_CACHE}/*/*/site-packages"
        "${OME_EP_TOOL_DIR}/*/site-packages"
        "${OME_EP_TOOL_DIR}/*/*/site-packages"
-       "${OME_EP_LIB_DIR}/*/site-packages")
+       "${OME_EP_INSTALL_DIR}/lib/*/site-packages"
+       "${OME_EP_INSTALL_DIR}/lib64/*/site-packages")
   foreach(dir ${python_dirs})
     if(PYTHONPATH)
       set(PYTHONPATH "${dir}:${PYTHONPATH}")
