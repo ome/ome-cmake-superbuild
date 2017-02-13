f5a952541a43253610f7a2dbf3145053eff5eddf
Roger Leigh <rleigh@dundee.ac.uk>
2017-02-13 10:42:50 +0000

    Embed release version directly in CMakeLists.txt

--- packages/ome-cmake-superbuild-docs-contents/superbuild.cmake
+++ packages/ome-cmake-superbuild-docs-contents/superbuild.cmake
@@ -13,9 +13,10 @@ ome_add_dependencies(ome-cmake-superbuild-docs-contents
 unset(ome_docs_dependencies)
 
 list(APPEND CONFIGURE_OPTIONS
-     "-DDOC_VERSION_MAJOR=${OME_VERSION_MAJOR}"
-     "-DDOC_VERSION_MINOR=${OME_VERSION_MINOR}"
-     "-DDOC_VERSION_PATCH=${OME_VERSION_PATCH}"
+     "-DDOC_VERSION_MAJOR=${ome-cmake-superbuild_VERSION_MAJOR}"
+     "-DDOC_VERSION_MINOR=${ome-cmake-superbuild_VERSION_MINOR}"
+     "-DDOC_VERSION_PATCH=${ome-cmake-superbuild_VERSION_PATCH}"
+     "-DDOC_VERSION_DEV=${OME_VCS_DEV}"
      ${SUPERBUILD_OPTIONS})
 string(REPLACE ";" "^^" CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS}")
 
