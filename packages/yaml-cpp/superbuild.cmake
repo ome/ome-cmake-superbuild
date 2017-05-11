61df864b6e5debd3dec194182831e535736e8c85
Roger Leigh <rleigh@dundee.ac.uk>
2017-05-11 10:13:11 +0100

    yaml-cpp: Add patch to build with gtest 1.8.0

--- packages/yaml-cpp/superbuild.cmake
+++ packages/yaml-cpp/superbuild.cmake
@@ -1,9 +1,10 @@
 # yaml-cpp superbuild
 
 # Set dependency list
-ome_add_dependencies(yaml-cpp THIRD_PARTY_DEPENDENCIES boost gtest)
+ome_add_dependencies(yaml-cpp THIRD_PARTY_DEPENDENCIES boost gtest patch)
 
 list(APPEND CONFIGURE_OPTIONS -Wno-dev --no-warn-unused-cli)
+list(APPEND CONFIGURE_OPTIONS "-DGTEST_SOURCE=${CMAKE_BINARY_DIR}/gtest-source")
 string(REPLACE ";" "^^" CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS}")
 
 ExternalProject_Add(${EP_PROJECT}
@@ -13,6 +14,13 @@ ExternalProject_Add(${EP_PROJECT}
   SOURCE_DIR "${EP_SOURCE_DIR}"
   BINARY_DIR "${EP_BINARY_DIR}"
   INSTALL_DIR ""
+  PATCH_COMMAND
+    ${CMAKE_COMMAND}
+    "-DSOURCE_DIR:PATH=${EP_SOURCE_DIR}"
+    "-DPATCH_DIR:PATH=${CMAKE_CURRENT_LIST_DIR}/patches"
+    "-DCONFIG:INTERNAL=$<CONFIG>"
+    "-DEP_SCRIPT_CONFIG:FILEPATH=${EP_SCRIPT_CONFIG}"
+    -P "${GENERIC_PATCH}"
   CONFIGURE_COMMAND ${CMAKE_COMMAND}
     "-DSOURCE_DIR:PATH=${EP_SOURCE_DIR}"
     "-DBUILD_DIR:PATH=${EP_BINARY_DIR}"
