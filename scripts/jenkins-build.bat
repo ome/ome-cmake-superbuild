b3de83745888cf1638164ea23f8241c74d0cff9d
Roger Leigh <rleigh@codelibre.net>
2017-06-06 17:20:46 +0100

    scripts: Don't force build of yaml; require opt-in by packages

--- scripts/jenkins-build.bat
+++ scripts/jenkins-build.bat
@@ -34,7 +34,7 @@ set parallel_opt=OFF
 set build_git=OFF
 set action=build
 set qt=OFF
-set "packages=ome-files;ome-cmake-superbuild-docs;ome-cmake-superbuild-docs-contents;yaml-cpp"
+set "packages=ome-files;ome-cmake-superbuild-docs;ome-cmake-superbuild-docs-contents"
 
 REM Parse command line options.
 :loop
