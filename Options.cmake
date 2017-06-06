bc3dd862916ec95e60579ea88bc55ff6022d147a
Roger Leigh <rleigh@codelibre.net>
2017-06-06 20:09:49 +0100

    cmake: Use C++ standard variables as documented

--- Options.cmake
+++ Options.cmake
@@ -83,8 +83,8 @@ option(sphinx "Enable sphinx manual page and HTML documentation" ON)
 option(sphinx-linkcheck "Check sphinx documentation links by default" OFF)
 
 # C++ standard
-set(CMAKE_CXX_STANDARD "14" CACHE STRING "Preferred C++ standard version")
-set(CMAKE_CXX_STANDARD_REQUIRED "11" CACHE STRING "Minimum C++ standard version")
+set(CMAKE_CXX_STANDARD "14" CACHE STRING "Preferred C++ standard version (will fall back to earlier versions if unavailable)")
+set(CMAKE_CXX_STANDARD_REQUIRED FALSE CACHE BOOL "Force use of specified C++ standard (no fallback to earlier versions)")
 
 set(SUPERBUILD_OPTIONS
     "-Dextra-warnings:BOOL=${extra-warnings}"
